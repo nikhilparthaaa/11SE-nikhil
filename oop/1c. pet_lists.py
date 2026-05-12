9fba0e5370e6cddbb8f5c42de6db6613fb331116
nikhilparthaaa <nikhil.parthasarathy@education.nsw.gov.au>
2026-05-12 04:40:52 +0000

    3.a

--- oop/1c. pet_lists.py
+++ oop/1c. pet_lists.py
@@ -1,6 +1,18 @@
 #Tutorial 3 Lists:
 #1. Create an example of parallel lists eg: pet_name, species, age, vaccination_status for three pets
-def__init__():
+pet_name = ['Jack', 'jenny', 'ovuevuevue']
+species = ['dog', 'cat', 'hamster']
+age = [5,8,1]
+vaccination_status = [True, False, True]
+
+for i in range(len(pet_name)):
+    print(pet_name[i])
+    print(species[i])
+    print(age[i])
+    print(vaccination_status[i])
+    print('')
+
+
 #2. Use a for loop to print parallel list details. This will mean that one complete printout will look like:
 '''
 Pet name: Foxy
@@ -15,9 +27,18 @@ Vaccination Status: False
 
   #ACTIVITIES:
 # In each activity test out that the printing of data is still valid
-
 #1. Add a new animal named Hootie, its a blowfish, it is 34 years
 #2. Vaccinate an unvaccinated animal (create vaccination)
 #3. Remove an animal and make sure that all the printing is correct
 
-print("")
+pet_name = ['Jack', 'jenny', 'ovuevuevue', 'hootie']
+species = ['dog', 'cat', 'hamster', 'blowfish']
+age = [5,8,1,13]
+vaccination_status = [True, False, True, False]
+
+for i in range(len(pet_name)):
+    print(pet_name[i])
+    print(species[i])
+    print(age[i])
+    print(vaccination_status[i])
+    print('')
--- oop/3a. pet_class_activities.py
+++ oop/3a. pet_class_activities.py
@@ -2,9 +2,25 @@
 # - Create a class pet with same information as in previous examples
 # - Create an object instance of class pet
 
+class pet:
+    def __init__(self, pet_name, category, age, vaccine, credit_card, billing_address, owner_name, account_balance):
+        self.name=pet_name
+        self.category = category
+        self.age = age
+        self.vaccine = vaccine
+        self.credit = credit_card
+        se;f.billing = billing_address
+        self.owner = owner_name
+        self.account = account_balance
 
+p1=pet('Bonnie', 'cat', 12, True)
 
+print(p1.name)
+print(p1.category)
+print(p1.age)
+print('vaccinated:',p1.vaccine)
 
+p2=pet('Foxy', "dog", '15', "False")
 #ACTIVITIES:
 #1. Print out vaccination status of Bonnie
 #2. Create another pet named Foxy who is a dog
